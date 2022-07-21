f9db3e11dab3a4b3cc44e0bf589a4d245e27b5de
nurahasny <nura.alhasani@gmail.com>
2022-07-21 13:29:25 +0700

    pemabayaran listrik

---
+++ config/koneksi.php
@@ -0,0 +1,4 @@
+<?php  
+	mysql_connect("localhost","root","");
+	mysql_select_db("pembayaran_listrik");
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<br> <br> <br> <br>  
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 Not Found</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body style="background-color: #eeeeee;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">404 Not Found</div>
+					<div class="panel-body">
+						<h3>Maaf Halaman Tidak Ada :(</h3>
+						<h4><a class="btn btn-warning" href="pln">PLN</a> | <a class="btn btn-primary" href="agen">AGEN</a></h4>
+					</div>
+					<div class="panel-footer">
+						<center>&copy;2022 - NURA HANSY | Distributed by </center>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
